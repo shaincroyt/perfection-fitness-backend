fb68b44dac7ab14c1e6fa622fac13fe89bdc14ce
Lucas Jara <lucasjara1212@gmail.com>
2026-06-05 03:59:29 -0500

    Corrige estilos profile y logout en dashboard

--- admin-assets/js/perfil.js
+++ admin-assets/js/perfil.js
@@ -105,6 +105,7 @@
         padding: 10px 0;
         border-bottom: 1px solid rgba(232,233,236,.75);
         align-items: center;
+        margin-bottom: 10px;
       }
 
       .profile-field:last-child { border-bottom: 0; }
