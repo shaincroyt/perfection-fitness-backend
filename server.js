75f5bae27581c1a733b88611aadd12b5aea969fe
Lucas Jara <lucasjara1212@gmail.com>
2026-06-06 00:04:43 -0500

    Responsive admin y correcciones de modales

--- server.js
+++ server.js
@@ -2262,6 +2262,6 @@ app.get('/api/test', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
